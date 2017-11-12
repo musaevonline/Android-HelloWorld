d6bfcc8248d37191d021291a83972773e55215df
PC1-linux <greycat9515@gmail.com>
2017-11-12 22:06:54 +0300

    First

---
+++ Documents/Android/P001_HelloWorld/app/src/main/java/com/example/greycat/p001_helloworld/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.greycat.p001_helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
